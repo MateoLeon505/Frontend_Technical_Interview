6f80ea1ceddfd556a5ed1c1fd98a39b8bf72988d
mateoleont <mateolt505@gmail.com>
2024-03-22 12:15:48 -0500

    Ejercicio 3 de lógica finalizado

--- logica/3.js
+++ logica/3.js
@@ -11,12 +11,37 @@
    🪣💧🪣🪣💧🪣
    🪣💧🪣🪣💧🪣
    🪣💧🪣🪣🪣🪣
- 
+  
    Representando bloque con 🪣︎ y agua con 💧, quedarán atrapadas 7 unidades
    de agua. Suponemos que existe un suelo impermeable en la parte inferior
    que retiene el agua.
  */
 
 const waterBlocks = (value) => {
-  return;
+  const length = value.length;
+  if (length < 3) return 0;
+  let trappedWater = 0;
+  const maxLeftBlock = new Array(length).fill(0);
+  const maxRightBlock = new Array(length).fill(0);
+
+  maxLeftBlock[0] = value[0];
+  maxRightBlock[length - 1] = value[length - 1];
+
+  for (let i = 1; i < length; i++) {
+    maxLeftBlock[i] = Math.max(maxLeftBlock[i - 1], value[i]);
+  }
+
+  for (let i = length - 2; i >= 0; i--) {
+    maxRightBlock[i] = Math.max(maxRightBlock[i + 1], value[i]);
+  }
+
+  for (let i = 0; i < length; i++) {
+    trappedWater += Math.max(
+      0,
+      Math.min(maxLeftBlock[i], maxRightBlock[i]) - value[i]
+    );
+  }
+
+  return trappedWater;
 };
+
