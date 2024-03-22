7e97851022444ec205325d8239aa07cc21211a1f
mateoleont <mateolt505@gmail.com>
2024-03-21 23:05:44 -0500

    Ejercicio de lógica 2 terminado.

--- logica/2.js
+++ logica/2.js
@@ -14,5 +14,44 @@
  */
 
 const t9Keyboard = (value) => {
-  return;
+  const keyboardPrediction = {
+    2: "A",
+    22: "B",
+    222: "C",
+    3: "D",
+    33: "E",
+    333: "F",
+    4: "G",
+    44: "H",
+    444: "I",
+    5: "J",
+    55: "K",
+    555: "L",
+    6: "M",
+    66: "N",
+    666: "O",
+    7: "P",
+    77: "Q",
+    777: "R",
+    7777: "S",
+    8: "T",
+    88: "U",
+    888: "V",
+    9: "W",
+    99: "X",
+    999: "Y",
+    9999: "Z",
+    0: " "
+  };
+  let text = [];
+  const sets = value.split("-");
+
+  sets.forEach((set) => {
+    const letter = keyboardPrediction[set];
+    letter && text.push(letter);
+  });
+
+  text = text.join("");
+  return text;
 };
+
