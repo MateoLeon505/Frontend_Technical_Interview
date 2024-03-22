6f258fa9d930066b3b7b67ee24392348b8de1320
mateoleont <mateolt505@gmail.com>
2024-03-21 19:25:47 -0500

    Ejercicio de lógica 1 terminado.

--- logica/1.js
+++ logica/1.js
@@ -1,4 +1,7 @@
-// Debes escribir un programa que encuentre la frecuencia con que aparecen los distintos caracteres (letras y números) dentro de una cadena de texto. El resultado se devuelve una matriz de objetos. Cada uno de estos objetos tiene 2 campos: carácter (caracter) y número de veces que aparece (veces). La matriz estará ordenada por el campo carácter. No se diferencia entre mayúsculas y minúsculas ni entre vocales acentuadas.
+// Debes escribir un programa que encuentre la frecuencia con que aparecen los distintos caracteres
+// (letras y números) dentro de una cadena de texto. El resultado se devuelve una matriz de objetos.
+// Cada uno de estos objetos tiene 2 campos: carácter (caracter) y número de veces que aparece (veces).
+// La matriz estará ordenada por el campo carácter. No se diferencia entre mayúsculas y minúsculas ni entre vocales acentuadas.
 
 // Ejemplo:
 
@@ -18,5 +21,27 @@
 // {car: 'y', veces: 2}
 
 const contarCar = (value) => {
-  return;
+  const frequency = [];
+
+  const characters = value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split("")
+    .filter((character) => character !== " ");
+
+  characters.map((character) => {
+    const repeated = frequency.some((object) => object.car === character);
+    const counter = characters.filter((char) => char === character).length;
+    !repeated &&
+      frequency.push({
+        car: character,
+        veces: counter,
+      });
+  });
+
+  frequency.sort((a, b) => a.car.localeCompare(b.car));
+
+  return frequency;
 };
+
