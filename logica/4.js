429b6224f0821d1fcbd69a8909499de63b2f9374
mateoleont <mateolt505@gmail.com>
2024-03-23 23:59:11 -0500

    lectura código

--- logica/4.js
+++ logica/4.js
@@ -1,4 +1,9 @@
-// Lee detallada mente la imagen "./code_example.png" y realiza un analisis de su funcionalidad, y como lo podrias mejorar
+// Lee detalladamente la imagen "./code_example.png" y realiza un analisis de su funcionalidad, y como lo podrias mejorar
 //Pista, el codigo presentado handlea dos diferentes tipos de tokens (api token, session token)
 
 import image from "./code_example.png";
+// El código gestiona la autenticación y autorización del usuario en la página.
+
+// * updateCurrency: Es la función que actualiza el valor de la moneda, hace llamado a la api para recibir datos sobre
+//   variación en las tasas de cambio y usa el almacenamiento local del navegador para registrar los cambios.
+
