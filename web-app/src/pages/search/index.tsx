f6932274ef4ee722bf66eb5fe3aa0a6d1dc347f3
mateoleont <mateolt505@gmail.com>
2024-03-26 02:45:14 -0500

    casi listo

--- web-app/src/pages/search/index.tsx
+++ web-app/src/pages/search/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Succes } from "@/components/index";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>Search
+      <Succes/>
+    </div>
+  );
 };
 
 export default Search;
